417dc13a92120727b7155d12a723d21733fd1fb1
harguello <hectorargue@gmail.com>
2015-06-03 15:20:37 +0200

    Nuevos controllers

---
+++ application/controllers/Benefactores.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+
+class Benefactores extends CI_Controller {
+    //put your code here
+    
+    public function __construct() {
+        parent::__construct();
+        $idiom = (empty($this->session->idiom)) ?  $this->session->idiom : $this->config->item('language');
+         $this->lang->load('menu', $idiom);
+    }
+    
+    public function index(){
+        $this->load->view('templates/head');
+                $this->load->view('templates/header');
+        $this->load->view('inscripcion_benefactores');
+        $this->load->view('templates/footer');
+        $this->load->view('templates/scripts');
+    }
+}
